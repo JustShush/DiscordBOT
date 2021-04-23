f0c48ab0b28d06802d1ec7f0bebadbc39ca28bb0
JustShush <70417913+JustShush@users.noreply.github.com>
2021-04-23 21:36:19 +0100

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "this sends help!",
     execute(client, message, args, Discord) {
         setInterval(() => {
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 1; i++) {
                 const channel = client.channels.cache.find(channel => channel.name === "spam")
                 channel.send("+teste")
                 //console.log("teste" + i )
