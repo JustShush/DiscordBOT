826a7c3c3aea513691d91b5c05bd867536509e9c
JustShush <70417913+JustShush@users.noreply.github.com>
2021-05-01 16:27:30 +0100

    Add files via upload

---
+++ commands/commands.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'commands',
+    permissions: [],
+    description: "this sends help!",
+    execute(client, message, args, Discord) {
+        const newEmbed = new Discord.MessageEmbed()
+            .setColor('#7e11dd')
+            .setTitle('**Commands**')
+            .addFields({
+                    name: 'Vote for the server',
+                    value: '`+vote`',
+                    inline: true
+                }, {
+                    name: 'Check your ping',
+                    value: '`+ping`',
+                    inline: true
+                }, {
+                    name: '\u200B',
+                    value: '\u200B',
+                    inline: true
+                }, {
+                    name: 'Quem é que é GAY?',
+                    value: '`+quemequeegay`',
+                    inline: true
+                }, {
+                    name: 'Who is Best Girl?',
+                    value: '`+whoisbestgirl`',
+                    inline: true
+                }, )
+
+            
+        message.channel.send(newEmbed);
+    }
+
+}
